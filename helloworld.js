39a30ed826ef05f543a54fa20a6c4f415db66cd5
Andrew Sinemoglou <63246095+asinemog@users.noreply.github.com>
2021-06-14 18:09:53 +1000

    Add files via upload

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+function getfib(){
+
+    var index = document.getElementById("fibnum").value;
+     
+    var x = 1;
+    var y = 0;
+    var temp = 0;
+    var count = 0;
+
+    while(count < index - 1){
+        temp = x;
+        x = x + y;
+        y = temp;
+        count++;
+    }
+
+    document.getElementById("cummies").innerHTML = y;
+}
